3d6d2dd9b7d4f23ca7e9dfbc3c8a4a047b1fb39b
Tu Nombre <tu.email@dominio.com>
2026-05-03 20:26:01 -0500

    fix: add tailwindcss vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
 });
